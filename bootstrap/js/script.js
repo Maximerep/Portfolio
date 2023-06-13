82c9ac99346189e2ed0ed91409f1a438fd9d5587
Maximerep <133568274+Maximerep@users.noreply.github.com>
2023-06-13 11:15:19 +0200

    ci . js

--- bootstrap/js/script.js
+++ bootstrap/js/script.js
@@ -1,26 +1,37 @@
+// Définir les mots-clés et leur fréquence
 var words = [
-    { text: "Musique", weight: 8 },
-    { text: "Voyages", weight: 6 },
-    { text: "Photographie", weight: 7 },
-    { text: "Design", weight: 5 },
-    { text: "Cuisine", weight: 6 },
-    { text: "Sport", weight: 4 },
-    { text: "Technologie", weight: 7 }
-    // Ajoutez d'autres mots ici avec leurs poids
+    { text: "Design", weight: 10 },
+    { text: "Développement web", weight: 8 },
+    { text: "Photographie", weight: 6 },
+    { text: "UI/UX", weight: 7 },
+    // Ajoutez plus de mots-clés avec leurs fréquences
   ];
   
-  // Options de configuration du nuage de mots
+  // Configurer les options du nuage de mots
   var options = {
-    list: words,
+    list: words.map(function(word) {
+      return [word.text, word.weight];
+    }),
     gridSize: 10,
-    weightFactor: 40,
+    weightFactor: 5,
     fontFamily: "Arial, sans-serif",
     color: "random-dark",
-    backgroundColor: "#f2f2f2",
+    backgroundColor: "#fff",
     rotateRatio: 0.5,
-    shape: "square",
-    shuffle: true
+    shape: "circle",
+    ellipticity: 0.65,
+    shuffle: true,
+    hover: function(item) {
+      // Action lorsqu'un mot est survolé
+      console.log(item);
+    },
+    click: function(item) {
+      // Action lorsqu'un mot est cliqué
+      console.log(item);
+      // Redirigez vers une autre page ou effectuez une action supplémentaire
+    },
   };
   
-  // Créer le nuage de mots
-  WordCloud(document.getElementById("word-cloud"), options);
+  // Générer le nuage de mots
+  WordCloud(document.getElementById("wordcloud"), options);
+  
