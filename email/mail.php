64b62aa2613a615513dd788781e7444023041387
Maximerep <133568274+Maximerep@users.noreply.github.com>
2023-06-14 09:54:12 +0200

    mailV2

--- email/mail.php
+++ email/mail.php
@@ -1,26 +1,36 @@
 <?php
-// Vérifier si le formulaire a été soumis
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Récupérer les données du formulaire
-    $nom = $_POST['nom'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+// Inclure le fichier d'autoloader de PHPMailer
+require 'email.PHPMailer.php';
 
-    // Ici, vous pouvez effectuer les opérations de traitement nécessaires
-    // Par exemple, envoyer un e-mail ou enregistrer les données dans une base de données
+// Créer une nouvelle instance de PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
 
-    // Exemple d'envoi d'e-mail de notification
-    $destinataire = 'max.repincay@gmail.com';
-    $sujet = 'Nouveau message de contact portfolio';
-    $contenu = "Nom: $nom\n";
-    $contenu .= "Email: $email\n";
-    $contenu .= "Message: $message\n";
+// Configurer les paramètres d'envoi d'e-mails
+$mail->isSMTP();
+$mail->Host = 'REPINCAY Maxime'; // Remplacez par le nom d'hôte du serveur SMTP
+$mail->SMTPAuth = true;
+$mail->Username = 'max.repincay@gmail.com'; // Remplacez par votre adresse e-mail
+$mail->Password = 'Portfolio'; // Remplacez par votre mot de passe
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
 
-    // Envoyer l'e-mail
-    mail($destinataire, $sujet, $contenu);
+// Récupérer les données du formulaire
+$nom = $_POST['nom'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
-    // Rediriger l'utilisateur vers une page de confirmation
-    header('Location: confirmation.html');
-    exit;
+// Configurer les détails du message
+$mail->setFrom($email, $nom); // Utiliser l'e-mail et le nom de l'expéditeur fournis dans le formulaire
+$mail->addAddress('destinataire@example.com', 'Nom du Destinataire'); // Remplacez par l'adresse e-mail du destinataire et son nom
+$mail->Subject = 'Nouveau message de contact'; // Remplacez par le sujet de l'e-mail
+$mail->Body = $message; // Utiliser le contenu du message fourni dans le formulaire
+
+// Envoyer l'e-mail
+if ($mail->send()) {
+    // L'e-mail a été envoyé avec succès
+    echo 'E-mail envoyé avec succès !';
+} else {
+    // Une erreur s'est produite lors de l'envoi de l'e-mail
+    echo 'Erreur lors de l\'envoi de l\'e-mail : ' . $mail->ErrorInfo;
 }
 ?>
